f98581f5522bd90c6cb3bf9c25d639c43da60491
Filipe Rother de Souza <filiperother@icloud.com>
2025-12-23 13:46:37 -0300

    fix html bg color

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scroll-smooth">
+    <html lang="en" className="scroll-smooth bg-secondary">
       <body className={`${geist.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
