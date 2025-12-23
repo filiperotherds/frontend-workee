d21be5b37a8e695f95b15b3c847859a5a767bb6a
Filipe Rother de Souza <filiperother@icloud.com>
2025-12-23 13:42:41 -0300

    change mobile header and content

--- app/(org-app)/layout.tsx
+++ app/(org-app)/layout.tsx
@@ -6,6 +6,7 @@ import type { Metadata } from "next";
 import { redirect } from "next/navigation";
 
 import Topbar from "@/components/topbar";
+import { ChartNoAxesColumnIncreasing } from "lucide-react";
 
 export const metadata: Metadata = {
   title: "Jobble",
@@ -28,15 +29,15 @@ export default async function OrgAppLayout({
 
   return (
     <div className="w-full">
-      <Topbar />
-      <div className="w-full h-full pt-[40px] md:pt-10">
-        <Header />
+      <div className="w-full h-full">
+        <Header icon={ChartNoAxesColumnIncreasing} pageTitle="Dashboard"/>
         <SidebarProvider className="min-h-[calc(100svh-64px)] md:min-h-[calc(100svh-104px)]">
           <AppSidebar />
-          <main className="w-full">
-            <div className="w-full p-4 gap-4 items-start justify-start flex flex-col md:flex-row">
-              <SidebarTrigger />
+          <main className="w-full bg-secondary">
+            <div className="w-full p-4 gap-4 items-start justify-start flex flex-col md:flex-row bg-white rounded-t-3xl">
+              <SidebarTrigger className="hidden md:flex"/>
               {children}
+              
             </div>
           </main>
         </SidebarProvider>
--- components/header.tsx
+++ components/header.tsx
@@ -1,29 +1,26 @@
-import Image from "next/image";
-import Link from "next/link";
-import jobbleWordmark from "@/public/jobble-wordmark.svg";
-import { ProfileButton } from "./profile-button";
-import Notification from "./notification";
+import { ProfileButton } from './profile-button';
+import Notification from './notification';
+import { LucideIcon } from 'lucide-react';
 
-export default function Header() {
-  return (
-    <div className="sticky top-0 z-20 w-full h-16 px-8 flex items-center justify-between rounded-t-lg bg-white shadow-sm">
-      <div className="flex items-center justify-center">
-        <Link href="/sign-in" className="flex flex-row items-end justify-start gap-2">
-          <Image
-            src={jobbleWordmark}
-            alt="Jobble"
-            className="w-20 dark:invert"
-          />
-          <span className="text-sm font-bold text-muted-foreground">
-            Beta
-          </span>
-        </Link>
-      </div>
+type HeaderProps = {
+	pageTitle: string;
+	icon: LucideIcon;
+};
 
-      <div className="flex items-center space-x-4 md:space-x-8">
-        <Notification />
-        <ProfileButton />
-      </div>
-    </div>
-  );
+export default function Header({ pageTitle, icon: Icon }: HeaderProps) {
+	return (
+		<div className='md:sticky top-0 z-20 w-full h-16 px-8 flex items-center justify-between rounded-t-lg bg-secondary'>
+			<div className='flex flex-row items-center justify-center gap-2'>
+				<Icon size={16} strokeWidth={4} className='text-[#8A8AA3]'/>
+				<span className='font-medium'>{pageTitle}</span>
+			</div>
+
+			<div className='flex items-center'>
+				<Notification />
+				<div className='hidden md:flex md:ml-8'>
+					<ProfileButton />
+				</div>
+			</div>
+		</div>
+	);
 }
