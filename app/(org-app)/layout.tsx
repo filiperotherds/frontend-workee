0159f1d443d8ab897e74a1c6167770c19c5dbb67
Filipe Rother de Souza <filiperother@icloud.com>
2025-12-23 13:44:39 -0300

    fix bg color

--- app/(org-app)/layout.tsx
+++ app/(org-app)/layout.tsx
@@ -28,16 +28,16 @@ export default async function OrgAppLayout({
   }
 
   return (
-    <div className="w-full">
+    <div className="w-full bg-secondary">
       <div className="w-full h-full">
         <Header icon={ChartNoAxesColumnIncreasing} pageTitle="Dashboard"/>
         <SidebarProvider className="min-h-[calc(100svh-64px)] md:min-h-[calc(100svh-104px)]">
           <AppSidebar />
-          <main className="w-full bg-secondary">
+          <main className="w-full">
             <div className="w-full p-4 gap-4 items-start justify-start flex flex-col md:flex-row bg-white rounded-t-3xl">
               <SidebarTrigger className="hidden md:flex"/>
               {children}
-              
+
             </div>
           </main>
         </SidebarProvider>
